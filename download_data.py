f354197f14a223de3eb3c9f1708db8c4a942343e
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-12-13 19:50:37 +1300

    Added functions to download and save down the price data.

--- download_data.py
+++ download_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import yfinance as yf
 
 
@@ -12,7 +13,37 @@ def load_etfs() -> pd.DataFrame:
     return etfs
 
 
-if __name__ == '__main__':
+def download_data(etfs: pd.DataFrame) -> pd.DataFrame:
+    """
+    Downloads data from Yahoo Finance for the given list of ETFs.
+
+    :etfs: list of ETFs as a pandas DataFrame
+    :returns: daily price data for the given list of ETFs.
+    """
+    tickers = ' '.join(etfs['Tickers'].to_list())
+    prices = yf.download(tickers, period='2y', interval='1d', group_by='ticker')
+    adj_closing_prices = []
+    for ticker in etfs['Tickers']:
+        adj_closing_prices.append(prices[ticker]['Adj Close'].to_list())
+    prices_df = pd.DataFrame(np.transpose(adj_closing_prices), columns=etfs['Tickers'])
+    return prices_df
+
+
+def save_to_csv(prices: pd.DataFrame, filename: str) -> None:
+    """
+    Saves the given DataFrame to a CSV file.
+
+    :prices: daily price data for the given list of ETFs.
+    :filename: name of the file to save the data to.
+    """
+    prices.to_csv(filename)
+
+
+def main():
     etfs = load_etfs()
-    for etf in etfs.values:
-        print(etf)
+    prices = download_data(etfs)
+    save_to_csv(prices, 'ETF_Prices.csv')
+
+
+if __name__ == '__main__':
+    main()
