73aacb141239045628e3889ad2c931f388e9512a
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-12-13 19:53:59 +1300

    Added function to load in the data.

--- optimisation.py
+++ optimisation.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import scipy.optimize as opt
+
 
 def sharpe_ratio(weights, returns):
     """
@@ -11,3 +14,20 @@ def sharpe_ratio(weights, returns):
     p_returns = np.sum((returns.mean()*weights*252))
     p_volatility = np.sqrt(np.dot(weights.T, np.dot(returns.cov()*252, weights)))
     return -p_returns/p_volatility
+
+
+def load_data(filename):
+    """
+    Loads the data from a CSV file.
+
+    :filename: string of the filename.
+    :return: pandas dataframe of the data.
+    """
+    return pd.read_csv(filename)
+
+
+if __name__ == '__main__':
+    # Load the data
+    data = load_data('ETF_Prices.csv')
+
+    print(data.head())
