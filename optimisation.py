0cbab0d9692f7a56e0377e4f9e100cf89ffb59d8
Giovanni-Stephens <stephens.giovanni@gmail.com>
2021-12-27 22:31:28 +1300

    Added PyGAD for another go at using a GA to solve this problem.

--- optimisation.py
+++ optimisation.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import scipy.optimize as opt
 from pyeasyga import pyeasyga
+import pygad
+
 
 MAX_NUM_STOCKS = 10
+data = None
 
 
 def sharpe_ratio(weights: np.array, returns: list, cov: list) -> float:
@@ -90,7 +93,20 @@ def fitness(individual, data):
         random_weights /= np.sum(random_weights)
         subset = data.iloc[np.array(individual).astype(bool), :]
         fitness = optimize(subset.transpose(), random_weights, max_weight=0.2)
+    else:
+        fitness = -np.count_nonzero(individual)
     return fitness
+
+
+def fitness_2(solution: np.array, solution_idx: int) -> float:
+    """
+    Fitness function for the pygad genetic algorithm.
+
+    :solution: binary array.
+    :solution_idx: int of the solution index.
+    """
+    fit = fitness(solution, data)
+    return fit
 
 
 def mutate(individual):
@@ -134,7 +150,7 @@ def crossover(parent_1, parent_2):
     return child_1, child_2
 
 
-def cardinality_constrained_optimisation(data: pd.DataFrame):
+def cardinality_constrained_optimisation(data: pd.DataFrame) -> list:
     """
     Performs the cardinality constrained optimisation.
 
@@ -156,6 +172,54 @@ def cardinality_constrained_optimisation(data: pd.DataFrame):
     return ga.best_individual()
 
 
+last_fitness = 0
+def on_generation(ga_instance: pygad.GA) -> None:
+    """
+    On each generation in the GA, this function is called.
+
+    :ga_instance: the GA instance.
+    """
+    global last_fitness
+    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]))
+    print("Change     = {change}".format(change=ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1] - last_fitness))
+    last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
+
+
+def cardinality_constrained_optimisation_2():
+    """
+    Performs the cardinality constrained optimisation.
+
+    :data: pandas dataframe of the returns data.
+    :return: the best Sharpe Ratio and the individual (portfolio).
+    """
+    ga_instance = pygad.GA(num_generations=25,
+                  initial_population=np.array([create_individual(data) for _ in range(500)]),
+                  num_parents_mating=100,
+                #   sol_per_pop=3000,
+                #   num_genes=data.shape[0],
+                  gene_type=int,
+                  init_range_low=0,
+                  init_range_high=2,
+                  mutation_probability=[0.9, 0.7],
+                  parent_selection_type='rank',
+                #   mutation_probability=0.1,
+                  random_mutation_min_val=-1,
+                  random_mutation_max_val=1,
+                  mutation_type="adaptive",
+                  crossover_type="single_point",
+                  crossover_probability=0.85,
+                  fitness_func=fitness_2,
+                  on_generation=on_generation,
+                  stop_criteria='saturate_3')
+    ga_instance.run()
+    solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+    print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+    return solution
+
+
 if __name__ == '__main__':
     prices_df = load_data('ETF_Prices.csv')
     prices_df = prices_df.drop(prices_df.columns[0], axis=1)
@@ -164,8 +228,10 @@ if __name__ == '__main__':
     # Fill the null values with the previous day's close price
     prices_df = prices_df.fillna(method='ffill')
     log_returns = calculate_returns(prices_df)
-    best_individual = cardinality_constrained_optimisation(log_returns)
-    print(best_individual[0])
-    print(fitness(best_individual[0], log_returns))
-    print(prices_df.iloc[:, np.array(best_individual[1]).astype(bool)].columns)
+    data = log_returns.transpose()
+    # best_individual = cardinality_constrained_optimisation(log_returns)
+    best_individual = cardinality_constrained_optimisation_2()
+    # print(best_individual[0])
+    print(fitness(best_individual, log_returns.T))
+    print(prices_df.iloc[:, np.array(best_individual).astype(bool)].columns)
     # print(best_individual)
