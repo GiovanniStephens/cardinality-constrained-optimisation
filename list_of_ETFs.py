fb64fd8b67b7733fdbfdcd057779bdfe694f3afd
GiovanniStephens <stephens.giovanni@gmail.com>
2024-08-24 15:30:29 +1200

    Removing unnecessary comment.

--- list_of_ETFs.py
+++ list_of_ETFs.py
@@ -1,12 +1,12 @@
-from etfpy import ETF, load_etf, get_available_etfs_list
+from etfpy import get_available_etfs_list
 import pandas as pd
+
 
 def main():
     """
     Main function.
     """
     etfs_list = get_available_etfs_list()
-    # Convert the list of ETFs to a pandas DataFrame
     etf_df = pd.DataFrame(etfs_list, columns=['Ticker'])
     etf_df.to_csv('Data/ETFs_.csv', index=False)
 
