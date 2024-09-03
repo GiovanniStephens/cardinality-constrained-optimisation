8cb5fdfbcfb796994f089894ef2146045c804d0f
GiovanniStephens <stephens.giovanni@gmail.com>
2024-09-04 09:27:32 +1200

    Adding functionality to use forecast returns. Adding random state to each thread to get different populations. Added function to optimise the weights and print the results.

--- simple_ga_optimisation.py
+++ simple_ga_optimisation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy.optimize as opt
 from multiprocessing import Pool, Manager
 import os
 
@@ -22,8 +23,18 @@ def calculate_variances(log_returns: pd.DataFrame) -> pd.Series:
     return log_returns.var() * 252
 
 
-def calculate_expected_returns(log_returns: pd.DataFrame) -> pd.Series:
-    return log_returns.mean() * 252
+def calculate_expected_returns(log_returns: pd.DataFrame, use_forecasts=False) -> pd.Series:
+    if use_forecasts:
+        expected_returns = pd.read_csv('Data/expected_returns.csv', index_col=0)
+        expected_returns = expected_returns[expected_returns.index.isin(log_returns.columns)]
+        # If the expected return is > 0.5, then calculate the mean of the log returns parameter instead
+        for ticker in expected_returns.index:
+            # if over 0.5 or nan
+            if expected_returns.loc[ticker, '0'] > 0.5 or np.isnan(expected_returns.loc[ticker, '0']):
+                expected_returns.loc[ticker, '0'] = log_returns[ticker].mean() * 252
+        return expected_returns['0']
+    else:
+        return log_returns.mean() * 252
 
 
 def calculate_covariance_matrix(log_returns: pd.DataFrame) -> pd.DataFrame:
@@ -49,7 +60,7 @@ def calculate_fitness(individual, expected_returns, cov_matrix):
     """ Calculate Sharpe Ratio as the fitness of the individual with constraints on holdings. """
     selected_indices = individual == 1
     num_selected_etfs = np.sum(selected_indices)
-    if num_selected_etfs < 3 or num_selected_etfs > 5:
+    if num_selected_etfs < 8 or num_selected_etfs > 20:
         return -1e4  # Penalize solutions that do not meet the holding constraints
     if not selected_indices.any():
         return 0  # Avoid division by zero if no ETFs are selected
@@ -106,10 +117,9 @@ def elitism(population, fitness, num_elites):
 
 def genetic_algorithm(island_id, data, num_generations, population_size, mutation_rate, num_elites, migration_interval, migration_rate, return_dict):
     num_etfs = data.shape[1]
-    population = initialize_population(population_size, num_etfs, max_num_etfs=10)
+    population = initialize_population(population_size, num_etfs, max_num_etfs=20)
     log_returns = calculate_returns(data)
     expected_returns = calculate_expected_returns(log_returns)
-    # cov_matrix = calculate_covariance_matrix(log_returns)
     best_overall_fitness = float('-inf')
     best_overall_individual = None
     for generation in range(num_generations):
@@ -145,7 +155,11 @@ def run_parallel_ga(data, num_generations, total_population_size, mutation_rate,
     num_islands = os.cpu_count()  # Number of islands equal to the number of CPU cores
     manager = Manager()
     return_dict = manager.dict()
-    with Pool(num_islands) as pool:
+    def init_random_state():
+        # Seed numpy's random number generator differently in each process
+        np.random.seed(None)  # This sets a random seed based on the system time or os.urandom
+
+    with Pool(num_islands, initializer=init_random_state) as pool:
         args = [(i, data, num_generations, total_population_size // num_islands, mutation_rate, num_elites, migration_interval, migration_rate, return_dict) for i in range(num_islands)]
         results = pool.starmap(genetic_algorithm, args)
     # Determine the best result
@@ -157,20 +171,52 @@ def run_parallel_ga(data, num_generations, total_population_size, mutation_rate,
             best_solution = solution
     return best_solution, best_fitness
 
+
+def optimise_weights(best_solution, data):
+    selected_etfs = data.columns[best_solution == 1]
+    data = data[selected_etfs]
+    log_returns = calculate_returns(data)
+    expected_returns = calculate_expected_returns(log_returns)
+    cov_matrix = calculate_covariance_matrix(log_returns)
+    # Maximise the Sharpe Ratio subject to a total weight constraint of 1 and individual weight constraints of min 0
+    num_etfs = len(selected_etfs)
+    bounds = [(0, 1) for _ in range(num_etfs)]
+    constraints = [{'type': 'eq', 'fun': lambda x: np.sum(x) - 1}]
+    constraints.append({'type': 'ineq', 'fun': lambda x: np.dot(expected_returns, x) - 0.12})
+    def objective(x):
+        return -(np.dot(expected_returns, x) / np.sqrt(np.dot(x, np.dot(cov_matrix, x))))
+    result = opt.minimize(objective, x0=np.ones(num_etfs) / num_etfs, bounds=bounds, constraints=constraints)
+    return result
+
+
+def print_results(tickers, optimal_weights, amount_to_allocate=5000):
+    for ticker, weight in zip(tickers, optimal_weights):
+        if weight < 1e-4:
+            continue
+        else:
+            print(f"{ticker}: {weight*100:.0f}% or ${weight*amount_to_allocate:.2f}")
+
 if __name__ == '__main__':    
     # Load data and run the GA
     data = load_data('Data/ETF_Prices.csv')
+    etfs = pd.read_csv('Data/ETFs.csv')
+    # Remove any ETF that is in etfs but not in data
+    etfs_in_data_but_not_in_etfs = set(data.columns) - set(etfs['Tickers'])
+    data = data.drop(columns=etfs_in_data_but_not_in_etfs)
     # data = data.iloc[:-213]
-    mutation_rate = 1/ data.shape[1]
+    mutation_rate = 1 / data.shape[1]
     best_solution, best_fitness = run_parallel_ga(data,
-                                                num_generations=30,
-                                                total_population_size=16000,
+                                                num_generations=50,
+                                                total_population_size=8000,
                                                 mutation_rate=mutation_rate,
                                                 num_elites=100,
-                                                migration_interval=50,
+                                                migration_interval=10,
                                                 migration_rate=0.1)
     print("Best Solution:", best_solution)
     print("Best Sharpe Ratio:", best_fitness)
+    # Optimize the weights of the selected ETFs
+    optimized_weights = optimise_weights(best_solution, data)
+    print_results(data.columns[best_solution == 1], optimized_weights.x)
 
     # Print the selected ETFs
     selected_etfs = data.columns[best_solution == 1]
